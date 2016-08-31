3da5c0d1d278b332d6736d3a973ed91a49b9b959
Tyler Gibbons <tyler.gibbons@ideogr.am>
2016-08-31 01:28:11 -0700

    Create project mixfile and get excheck as initial dependency

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule Stockfighter.Mixfile do
+  use Mix.Project
+
+  # Project metadata definition
+  def project do
+    [app:             :stockfighter,
+     version:         "0.1.0", 
+     elixir:          "~> 1.3", 
+     build_embedded:  Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps:            deps]
+  end
+
+  # OTP Application Config
+  def application do
+    [mod:          {Stockfighter, []},
+     applications: [:logger, :phoenix]]
+  end
+
+  # Dependencies
+  defp deps do
+    [{:excheck, "~> 0.5",              only: :test},
+     {:triq,    github: "triqng/triq", only: :test}]
+  end
+end
